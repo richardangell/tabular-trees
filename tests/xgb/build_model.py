06e8cec82582a352557a6224eef058a084090065
richardangell <richardangell37@gmail.com>
2020-06-25 13:02:24 +0100

    add test case that shapley values function gives expected output

--- tests/xgb/build_model.py
+++ tests/xgb/build_model.py
@@ -1,4 +1,6 @@
 import xgboost as xgb
+import pandas as pd
+import numpy as np
 from sklearn.datasets import load_boston
 
 
@@ -21,6 +23,46 @@ def build_depth_3_model(n_trees = 10):
         }, 
         dtrain = xgb_data, 
         num_boost_round = n_trees
+    )
+
+    return model
+
+
+
+def build_example_shap_model():
+    """Function to build simple model on 10 row dataset where expected shapley values are known."""
+
+    np.random.seed(100)
+    X_train = pd.DataFrame(
+        {
+            'x':[206]*5 + [194] + [6]*4,
+            'y': list(np.random.randint(100, 400, 6)) + [299, 299, 301, 301],
+            'z': list(np.random.randint(100, 400, 10))
+        }
+    )
+
+    Y_train = pd.Series([10]*5 + [20] + [50]*2 + [30]*2)
+    Y_train.name = 't'
+    
+    xgb_data = xgb.DMatrix(
+        data = X_train, 
+        label = Y_train
+    )
+    xgb_data.set_base_margin([0] * X_train.shape[0])
+
+    model = xgb.train(
+        params = {
+            'objective': 'reg:squarederror',
+            'max_depth': 2, 
+            'subsample': 1,
+            'colsample_bytree': 1,
+            'eta': 1, 
+            'lambda': 0,
+            'gamma': 0,
+            'alpha': 0
+        }, 
+        dtrain = xgb_data, 
+        num_boost_round = 1
     )
 
     return model
---
+++ tests/xgb/test_shapley_values.py
@@ -0,0 +1,32 @@
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+
+import build_model
+
+import pygbmexpl
+
+
+
+def test_expected_shapley_values():
+    """Function to test shapley values output are as expected for simple model."""
+
+    model = build_model.build_example_shap_model()
+
+    tree_df = pygbmexpl.xgb.parser.extract_model_predictions(model)
+    
+    row_to_explain = pd.Series({'x': 150, 'y': 75, 'z': 200})
+
+    shapley_values = pygbmexpl.xgb.explainer.shapley_values(tree_df, row_to_explain, False)
+
+    expected_values = {
+        'base': 23.0,
+        'tree': 0,
+        'x': -5.0,
+        'y': 2.0,
+        'z': 0.0
+    }
+
+    expected_shapley_values = pd.DataFrame(expected_values, index = [0])
+
+    pd.testing.assert_frame_equal(shapley_values, expected_shapley_values)
