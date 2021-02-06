e9df6a546054fcd0cd746ac24b31a00a8c8a4141
richardangell <richardangell37@gmail.com>
2021-02-06 21:03:59 +0000

    add python requires to setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setuptools.setup(
     url="https://github.com/richardangell/pygbmexpl",
     packages=setuptools.find_packages(),
     install_requires=required,
+    python_requires=">=3.7",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
