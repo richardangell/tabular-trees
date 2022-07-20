5ae6dcad61ec372b0b6f63080de5148b63f58828
richardangell <richardangell37@gmail.com>
2022-07-20 08:43:42 +0100

    add trees.BaseModelTabularTrees class

--- src/tabular_trees/trees.py
+++ src/tabular_trees/trees.py
@@ -1,8 +1,79 @@
 """Module for tree structure classes."""
 
 import pandas as pd
+from abc import ABC, abstractmethod
 
 from . import checks
+
+
+class BaseModelTabularTrees(ABC):
+    """Abstract base class for model specific TabularTrees classes.
+
+    Parameters
+    ----------
+    trees : pd.DataFrame
+        Model specific tree data in tabular structure.
+
+    """
+
+    trees: pd.DataFrame
+
+    def __init__(self, trees: pd.DataFrame) -> None:
+        pass
+
+    @classmethod
+    @property
+    @abstractmethod
+    def REQUIRED_COLUMNS(cls):
+        """REQUIRED_COLUMNS attribute that must be defined in classes
+        inheriting from BaseModelTabularTrees."""
+
+        raise NotImplementedError("REQUIRED_COLUMNS attribute not defined")
+
+    @classmethod
+    @property
+    @abstractmethod
+    def SORT_BY_COLUMNS(cls):
+        """SORT_BY_COLUMNS attribute that must be defined in classes inheriting
+        from BaseModelTabularTrees."""
+
+        raise NotImplementedError("SORT_BY_COLUMNS attribute not defined")
+
+    def __post_init__(self) -> None:
+        """Post init checks and processing.
+
+        Processing on the trees attribute is as follows;
+        - Columns are ordered into REQUIRED_COLUMNS order
+        - Rows are sorted by tree and node columns
+        - The index is reset and original index dropped.
+
+        Raises
+        ------
+        TypeError
+            If self.trees is not a pd.DataFrame.
+
+        ValueError
+            If REQUIRED_COLUMNS are not in self.trees.
+
+        """
+
+        checks.check_type(self.trees, pd.DataFrame, "trees")
+        checks.check_df_columns(self.trees, self.REQUIRED_COLUMNS)
+
+        # reorder columns and sort
+        self.trees = self.trees[self.REQUIRED_COLUMNS]
+        self.trees = self.trees.sort_values(self.SORT_BY_COLUMNS)
+
+        self.trees = self.trees.reset_index(drop=True)
+
+        self.__post__post__init__()
+
+    @abstractmethod
+    def __post__post__init__(self):
+        """Method to be called at the end of __post_init__ for model specific
+        processing."""
+
+        pass
 
 
 class TabularTrees:
