52e74e852ec21bee71595a0b37a9fe67ed726c4c
richardangell <richardangell37@gmail.com>
2022-07-22 19:31:07 +0100

    add tests for sklearn.trees.export_tree_data__hist_gradient_boosting_model function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from sklearn.datasets import load_diabetes
+from sklearn.datasets import load_diabetes, load_iris
 
 
 @pytest.fixture(scope="session")
@@ -8,5 +8,14 @@ def diabetes_data() -> pd.DataFrame:
     """Load the sklearn diabetes dataset."""
 
     data = load_diabetes()
+
+    return data
+
+
+@pytest.fixture(scope="session")
+def iris_data() -> pd.DataFrame:
+    """Load the sklearn iris dataset."""
+
+    data = load_iris()
 
     return data
--- tests/sklearn/conftest.py
+++ tests/sklearn/conftest.py
@@ -1,4 +1,7 @@
-from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.ensemble import (
+    HistGradientBoostingRegressor,
+    HistGradientBoostingClassifier,
+)
 from tabular_trees.sklearn.trees import _extract_hist_gbm_tree_data
 
 import pytest
@@ -21,3 +24,15 @@ def sklearn_diabetes_model_trees_dataframe(sklearn_diabetes_model):
     """Return the trees from sklearn_diabetes_model in DataFrame structure."""
 
     return _extract_hist_gbm_tree_data(sklearn_diabetes_model)
+
+
+@pytest.fixture(scope="session")
+def sklearn_iris_model(iris_data):
+    """Build an sklearn HistGradientBoostingClassifier with 10 trees and depth
+    3 on the iris dataset."""
+
+    model = HistGradientBoostingClassifier(max_iter=10, max_depth=3)
+
+    model.fit(iris_data["data"], iris_data["target"])
+
+    return model
