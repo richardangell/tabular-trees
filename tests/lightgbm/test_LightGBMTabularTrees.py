5792b639f2b66fb75a385edad6e19a960800a76b
richardangell <richardangell37@gmail.com>
2022-07-22 19:00:25 +0100

    move tests for LightGBMTabularTrees out of class

--- tests/lightgbm/test_LightGBMTabularTrees.py
+++ tests/lightgbm/test_LightGBMTabularTrees.py
@@ -4,74 +4,74 @@ from tabular_trees.trees import BaseModelTabularTrees
 from tabular_trees.lightgbm.trees import LightGBMTabularTrees
 
 
-class TestLightGBMTabularTreesInit:
-    """Tests for the LightGBMTabularTrees class."""
+def test_successfull_call(lgb_diabetes_model_trees_dataframe):
+    """Test successfull initialisation of the LightGBMTabularTrees class."""
 
-    def test_successfull_call(self, lgb_diabetes_model_trees_dataframe):
-        """Test successfull initialisation of the LightGBMTabularTrees class."""
+    LightGBMTabularTrees(lgb_diabetes_model_trees_dataframe)
 
-        LightGBMTabularTrees(lgb_diabetes_model_trees_dataframe)
 
-    def test_inheritance(self):
-        """Test that LightGBMTabularTrees inherits from BaseModelTabularTrees."""
+def test_inheritance():
+    """Test that LightGBMTabularTrees inherits from BaseModelTabularTrees."""
 
-        assert (
-            LightGBMTabularTrees.__mro__[1] is BaseModelTabularTrees
-        ), "ScikitLearnHistTabularTrees does not inherit from BaseModelTabularTrees"
+    assert (
+        LightGBMTabularTrees.__mro__[1] is BaseModelTabularTrees
+    ), "ScikitLearnHistTabularTrees does not inherit from BaseModelTabularTrees"
 
-    @pytest.mark.parametrize(
-        "attribute_name,expected_value",
-        [
-            ("SORT_BY_COLUMNS", ["tree_index", "node_depth", "node_index"]),
-            (
-                "REQUIRED_COLUMNS",
-                [
-                    "tree_index",
-                    "node_depth",
-                    "node_index",
-                    "left_child",
-                    "right_child",
-                    "parent_index",
-                    "split_feature",
-                    "split_gain",
-                    "threshold",
-                    "decision_type",
-                    "missing_direction",
-                    "missing_type",
-                    "value",
-                    "weight",
-                    "count",
-                ],
-            ),
-        ],
-    )
-    def test_sort_by_columns(
-        self, attribute_name, expected_value, lgb_diabetes_model_trees_dataframe
-    ):
-        """Test column related attributes are set as expected."""
 
-        assert (
-            getattr(LightGBMTabularTrees, attribute_name) == expected_value
-        ), f"{attribute_name} not expected on LightGBMTabularTrees class"
+@pytest.mark.parametrize(
+    "attribute_name,expected_value",
+    [
+        ("SORT_BY_COLUMNS", ["tree_index", "node_depth", "node_index"]),
+        (
+            "REQUIRED_COLUMNS",
+            [
+                "tree_index",
+                "node_depth",
+                "node_index",
+                "left_child",
+                "right_child",
+                "parent_index",
+                "split_feature",
+                "split_gain",
+                "threshold",
+                "decision_type",
+                "missing_direction",
+                "missing_type",
+                "value",
+                "weight",
+                "count",
+            ],
+        ),
+    ],
+)
+def test_sort_by_columns(
+    attribute_name, expected_value, lgb_diabetes_model_trees_dataframe
+):
+    """Test column related attributes are set as expected."""
 
-        tabular_trees = LightGBMTabularTrees(lgb_diabetes_model_trees_dataframe)
+    assert (
+        getattr(LightGBMTabularTrees, attribute_name) == expected_value
+    ), f"{attribute_name} not expected on LightGBMTabularTrees class"
 
-        assert (
-            getattr(
-                tabular_trees,
-                attribute_name,
-            )
-            == expected_value
-        ), f"{attribute_name} not expected on LightGBMTabularTrees object after initialisation"
+    tabular_trees = LightGBMTabularTrees(lgb_diabetes_model_trees_dataframe)
 
-    def test_trees_not_same_object(self, lgb_diabetes_model_trees_dataframe):
-        """Test the trees attribute is not the same object as that passed into
-        the init method."""
+    assert (
+        getattr(
+            tabular_trees,
+            attribute_name,
+        )
+        == expected_value
+    ), f"{attribute_name} not expected on LightGBMTabularTrees object after initialisation"
 
-        input_df = lgb_diabetes_model_trees_dataframe.copy()
 
-        tabular_trees = LightGBMTabularTrees(input_df)
+def test_trees_not_same_object(lgb_diabetes_model_trees_dataframe):
+    """Test the trees attribute is not the same object as that passed into
+    the init method."""
 
-        assert id(tabular_trees.trees) != id(
-            input_df
-        ), "trees attribute is the same object as passed into initialisation"
+    input_df = lgb_diabetes_model_trees_dataframe.copy()
+
+    tabular_trees = LightGBMTabularTrees(input_df)
+
+    assert id(tabular_trees.trees) != id(
+        input_df
+    ), "trees attribute is the same object as passed into initialisation"
