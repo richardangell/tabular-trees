bd5dabf401aba1325522d3f4731508947583d9bf
richardangell <richardangell37@gmail.com>
2022-07-23 08:01:08 +0100

    add tests for export_tree_data function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import pytest
-from sklearn.datasets import load_diabetes, load_iris
+from sklearn.datasets import load_diabetes, load_iris, load_breast_cancer
+from sklearn.ensemble import (
+    HistGradientBoostingRegressor,
+    HistGradientBoostingClassifier,
+)
 
 
 @pytest.fixture(scope="session")
@@ -19,3 +23,36 @@ def iris_data() -> pd.DataFrame:
     data = load_iris()
 
     return data
+
+
+@pytest.fixture(scope="session")
+def breast_cancer_data() -> pd.DataFrame:
+    """Load the sklearn breast cancer dataset."""
+
+    data = load_breast_cancer()
+
+    return data
+
+
+@pytest.fixture(scope="session")
+def sklearn_diabetes_model(diabetes_data):
+    """Build an sklearn HistGradientBoostingRegressor with 10 trees and depth
+    3 on the diabetes dataset."""
+
+    model = HistGradientBoostingRegressor(max_iter=10, max_depth=3)
+
+    model.fit(diabetes_data["data"], diabetes_data["target"])
+
+    return model
+
+
+@pytest.fixture(scope="session")
+def sklearn_breast_cancer_model(breast_cancer_data):
+    """Build an sklearn HistGradientBoostingClassifier with 10 trees and depth
+    3 on the breast cancer dataset."""
+
+    model = HistGradientBoostingClassifier(max_iter=10, max_depth=3)
+
+    model.fit(breast_cancer_data["data"], breast_cancer_data["target"])
+
+    return model
---
+++ tests/test_export_tree_data.py
@@ -0,0 +1,40 @@
+import pytest
+
+from tabular_trees.trees import export_tree_data
+from tabular_trees.sklearn.trees import ScikitLearnHistTabularTrees
+
+
+def test_successful_call(sklearn_diabetes_model):
+    """Test a successful call to export tree data."""
+
+    export_tree_data(sklearn_diabetes_model)
+
+
+def test_non_supported_type_exception():
+    """Test an NotImplementedError is raised if a non-supported type is passed
+    in the model argument."""
+
+    with pytest.raises(
+        NotImplementedError, match="model type not supported; <class 'int'>"
+    ):
+
+        export_tree_data(1)
+
+
+@pytest.mark.parametrize(
+    "model_fixture_name,expected_type",
+    [
+        ("sklearn_diabetes_model", ScikitLearnHistTabularTrees),
+        ("sklearn_breast_cancer_model", ScikitLearnHistTabularTrees),
+    ],
+)
+def test_model_specific_function_dispatch(request, model_fixture_name, expected_type):
+    """Test export_tree_data calls the correct model-speific export function."""
+
+    model = request.getfixturevalue(model_fixture_name)
+
+    tree_data = export_tree_data(model)
+
+    assert (
+        type(tree_data) is expected_type
+    ), f"incorrect type returned when export_tree_data called with {type(model)}"
