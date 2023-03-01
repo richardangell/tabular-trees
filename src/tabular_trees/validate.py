757ddf104d40ec9d31afa909a0243514098829ea
Richard Angell <richardangell37@gmail.com>
2023-03-01 20:28:13 +0000

    WIP: more tidying of validate_monotonic_constraints function

--- src/tabular_trees/validate.py
+++ src/tabular_trees/validate.py
@@ -1,5 +1,7 @@
 """Module for validating monotonic trends in trees."""
 
+from typing import Optional, Union
+
 import pandas as pd
 
 from .checks import check_condition, check_type
@@ -86,8 +88,10 @@ def _validate_monotonic_constraints(
                     tree_df["feature"] == constraint_variable, "node"
                 ].tolist()
 
-                # check all nodes below each node which splits on the variable of interest
-                # note, this could be made more efficient by
+                # check all nodes below each node which splits on the variable of
+                # interest
+                # note, this could be made more efficient by not rechecking lower nodes
+                # if they have been covered when checking a node above
                 for node_to_check in nodes_split_on_variable:
 
                     child_nodes_left: list = []
@@ -151,90 +155,58 @@ def _validate_monotonic_constraints(
 
 
 def _traverse_tree_down(
-    df,
-    node,
-    name,
-    nodes_list,
-    values_list,
-    value=None,
-    lower=None,
-    upper=None,
-    print_note="start",
-    verbose=False,
-):
-    """Find a value for variable of interest (name) that would end up in each node in the tree.
+    df: pd.DataFrame,
+    node: int,
+    name: str,
+    nodes_list: list,
+    values_list: list,
+    value: Optional[Union[int, float]] = None,
+    lower: Optional[Union[int, float]] = None,
+    upper: Optional[Union[int, float]] = None,
+) -> None:
+    """Find a value for variable of interest that would end up in each node in the tree.
 
     Parameters
     ----------
     df : pd.DataFrame
         Single tree structure output from parser.read_dump_text() or
         parser.read_dump_json().
+
     node : int
         Node number.
+
     name : str
         Name of variable of interest, function will determine values for this
         variable that would allow a data point to visit each node.
-    value : int, float or None
-        Value that has been sent to current node, default value of None is only
-        used at the top of the tree.
-    lower : int, float or None:
-        Lower bound for value of variable (name) that would allow a data point to
-        visit current node, default value of None is used from the top of the tree
-        until an lower bound is found i.e. right (no) split is traversed.
-    upper : int, float or None:
-        Upper bound for value of variable (name) that would allow a data point to
-        visit current node, default value of None is used from the top of the tree
-        until an upper bound is found i.e. left (yes) split is traversed.
-    print_note : str
-        Note to print if verbose = True, only able to set for first call of
-        function.
-    verbose : bool
-        Should notes be printed as function runs.
+
     nodes_list : list
         List to record the node numbers that are visited.
+
     values_list : list
-        List to record the values of variable (name) which allow each node to be
-        visited (same order as nodes_list).
+        List to record the values of variable {name} which allow each node to be
+        visited. In the same order as nodes_list.
+
+    value : int, float, default = None
+        Value that has been sent to current node, default value of None is only
+        used at the top of the tree.
+
+    lower : int, float, default =  None:
+        Lower bound for value of variable {name} that would allow a data point to
+        visit current node. Default value of None is used from the top of the tree
+        until an lower bound is found i.e. right (no) split is traversed.
+
+    upper : int, float, default =  None:
+        Upper bound for value of variable {name} that would allow a data point to
+        visit current node. Default value of None is used from the top of the tree
+        until an upper bound is found i.e. left (yes) split is traversed.
 
     Returns
     -------
-    No returns from function. However nodes_list (list) and values_list (list)
-    which are lists of nodes and corresponding data values of variable of
-    interest (name) that would allow each node to be visited - are updated as
-    the function runs
+    None. However nodes_list and values_list which are lists of nodes and corresponding
+    data values of variable of interest {name} that would allow each node to be visited
+     - are updated as the function descends the tree.
 
     """
-    if not isinstance(nodes_list, list):
-
-        raise TypeError("nodes_list must be a list")
-
-    if not isinstance(values_list, list):
-
-        raise TypeError("values_list must be a list")
-
-    if verbose:
-
-        print(
-            "_traverse_tree_down call;\n"
-            + "\tnode: "
-            + str(node)
-            + "\n"
-            + "\tname: "
-            + name
-            + "\n"
-            + "\tvalue: "
-            + str(value)
-            + "\n"
-            + "\tlower: "
-            + str(lower)
-            + "\n"
-            + "\tupper: "
-            + str(upper)
-            + "\n"
-            + "\tprint_note: "
-            + print_note
-        )
-
     # add the current node and value to lists to ultimately be returned from function
     nodes_list.append(node)
     values_list.append(value)
@@ -242,251 +214,188 @@ def _traverse_tree_down(
     # if we have reached a terminal node
     if df.loc[df["node"] == node, "leaf"].item() == 1:
 
-        if verbose:
-
-            print(
-                "node reached;\n"
-                + "\tnode: "
-                + str(node)
-                + "\n"
-                + "\tvalue: "
-                + str(value)
-                + "\n"
-                + "\tnode type: terminal node"
-            )
+        pass
 
     else:
 
-        if verbose:
+        # if the split for the current node is on the variable of interest update the
+        # following; value, lower, upper
+        if df.loc[df["node"] == node, "feature"].item() == name:
 
-            print(
-                "node reached;\n"
-                + "\tnode: "
-                + str(node)
-                + "\n"
-                + "\tvalue: "
-                + str(value)
-                + "\n"
-                + "\tnode type: internal node"
-            )
+            # pick a value and update bounds that would send a data point down the left
+            # (yes) split
 
-        # if the split for the current node is on the variable of interest update; value, lower, upper
-        if df.loc[df["node"] == node, "feature"].iloc[0] == name:
-
-            # pick a value and update bounds that would send a data point down the left (yes) split
-
-            # record the values for value, lower and upper when the function is called so they can be reset
-            # to these values before calling _traverse_tree_down down the no route within this if block
+            # record the values for value, lower and upper when the function is called
+            # so they can be reset to these values before calling _traverse_tree_down
+            # down the no route within this if block
             fcn_call_value = value
             fcn_call_lower = lower
             fcn_call_upper = upper
 
-            # if lower bound is unspecified, this means we have not previously travelled down a right path
-            # does not matter if upper bound is specified or not i.e. we have previously travelled down a left
-            # path, as going down the left path at the current node updates values in the same way
+            # if lower bound is unspecified, this means we have not previously
+            # travelled down a right path does not matter if upper bound is specified
+            # or not i.e. we have previously travelled down a left path, as going down
+            # the left path at the current node updates values in the same way
             if lower is None:
 
-                if verbose:
-
-                    print("values update for recursive call; \n\tcase i. lower None")
-
                 # choose a value above the split point to go down the left (yes) split
-                value = df.loc[df["node"] == node, "split_condition"].iloc[0] - 1
+                value = df.loc[df["node"] == node, "split_condition"].item() - 1
 
                 # there is no lower bound on values that will go down this path yet
                 lower = None
 
-                # the upper bound that can go down this split is the split condition (as we are now below it)
-                upper = df.loc[df["node"] == node, "split_condition"].iloc[0]
+                # the upper bound that can go down this split is the split condition
+                # (as we are now below it)
+                upper_split = df.loc[df["node"] == node, "split_condition"].item()
 
-            # if lower bound is specified, this means we have previously travelled down a right path
-            # does not matter if upper bound is specified or not i.e. we have previously travelled down a left
-            # path, as going down the left path at the current node updates values in the same way
+            # if lower bound is specified, this means we have previously travelled down
+            # a right path does not matter if upper bound is specified or not i.e. we
+            # have previously travelled down a left path, as going down the left path
+            # at the current node updates values in the same way
             else:
-
-                if verbose:
-
-                    print(
-                        "values update for recursive call; \n\tcase iii. lower not None and upper None"
-                    )
 
                 # lower bound remains the same
                 lower = lower
 
-                # but to send a data point to the left hand split the upper bound is the split condition
-                # for this node
-                upper = df.loc[df["node"] == node, "split_condition"].iloc[0]
+                # but to send a data point to the left hand split the upper bound is
+                # the split condition for this node
+                upper_split = df.loc[df["node"] == node, "split_condition"].item()
 
                 # set a value that falls between the bounds
-                value = (lower + upper) / 2
+                value = (lower + upper_split) / 2
 
             # recursively call function down the left child of the current node
             _traverse_tree_down(
                 df,
-                df.loc[df["node"] == node, "left_child"].iloc[0],
+                df.loc[df["node"] == node, "left_child"].item(),
                 name,
                 nodes_list,
                 values_list,
                 value,
                 lower,
-                upper,
-                "yes child - variable of interest",
-                verbose,
+                upper_split,
             )
 
-            # now pick a value and update bounds that would send a data point down the right (no) split
+            # now pick a value and update bounds that would send a data point down the
+            # right (no) split
 
-            # reset values as the if else blocks above will have changed them in order to go down the
-            # yes route with the right child node
+            # reset values as the if else blocks above will have changed them in order
+            # to go down the yes route with the right child node
             value = fcn_call_value
             lower = fcn_call_lower
             upper = fcn_call_upper
 
-            # if both upper bound is unspecified, this means we have not previously travelled down a left path
+            # if both upper bound is unspecified, this means we have not previously
+            # travelled down a left path
             if upper is None:
 
-                if verbose:
-
-                    print(
-                        "values update for recursive call; \n\tcase v. lower None and upper None"
-                    )
-
                 # choose a value above the split point to go down the right (no) split
-                value = df.loc[df["node"] == node, "split_condition"].iloc[0] + 1
+                value = df.loc[df["node"] == node, "split_condition"].item() + 1
 
-                # the lower bound that can go down this split is the split condition (as we are now above it)
-                lower = df.loc[df["node"] == node, "split_condition"].iloc[0]
+                # the lower bound that can go down this split is the split condition
+                # (as we are now above it)
+                lower_split = df.loc[df["node"] == node, "split_condition"].item()
 
                 # there is no upper bound on values that will go down this path yet
                 upper = None
 
             else:
 
-                if verbose:
-
-                    print(
-                        "values update for recursive call; \n\tcase vi. lower None and upper not None"
-                    )
-
                 # the lower bound becomes the split condition
-                lower = df.loc[df["node"] == node, "split_condition"].iloc[0]
+                lower_split = df.loc[df["node"] == node, "split_condition"].item()
 
                 # the upper bound remains the same
                 upper = upper
 
                 # set a value that falls between the bounds
-                value = (lower + upper) / 2
+                value = (lower_split + upper) / 2
 
             _traverse_tree_down(
                 df,
-                df.loc[df["node"] == node, "right_child"].iloc[0],
+                df.loc[df["node"] == node, "right_child"].item(),
                 name,
                 nodes_list,
                 values_list,
                 value,
-                lower,
+                lower_split,
                 upper,
-                "no child - variable of interest",
-                verbose,
             )
 
-        # otherwise, if the split for the current node is not on the variable of interest do not update values but
-        # continue down the tree
+        # otherwise, if the split for the current node is not on the variable of
+        # interest do not update values but continue down the tree
         else:
 
             _traverse_tree_down(
                 df,
-                df.loc[df["node"] == node, "left_child"].iloc[0],
+                df.loc[df["node"] == node, "left_child"].item(),
                 name,
                 nodes_list,
                 values_list,
                 value,
                 lower,
                 upper,
-                "yes child - variable not of interest",
-                verbose,
             )
 
             _traverse_tree_down(
                 df,
-                df.loc[df["node"] == node, "right_child"].iloc[0],
+                df.loc[df["node"] == node, "right_child"].item(),
                 name,
                 nodes_list,
                 values_list,
                 value,
                 lower,
                 upper,
-                "no child - variable not of interest",
-                verbose,
             )
 
 
-def gather__traverse_tree_down_results(nodes, values, name):
-    """Gather results from _traverse_tree_down into pd.DataFrame."""
-    if not isinstance(nodes, list):
-
-        raise TypeError("nodes must be a list")
-
-    if not isinstance(values, list):
-
-        raise TypeError("values must be a list")
-
-    if not isinstance(name, str):
-
-        raise TypeError("name must be a str")
-
-    if len(values) != len(nodes):
-
-        raise ValueError("nodes and values must be of the same length")
-
-    df = pd.DataFrame({"node": nodes, name + "_values": values})
-
-    if df["node"].duplicated().sum() > 0:
-
-        raise ValueError("duplicated nodes; " + str(nodes))
-
-    return df
-
-
 def _check_monotonicity_at_split(
-    tree_df, tree_no, trend, variable, node, child_nodes_left, child_nodes_right
-):
-    """Check monotonic trend is in place at a given split in a single tree."""
-    if not isinstance(tree_df, pd.DataFrame):
+    tree_df: pd.DataFrame,
+    tree_no: int,
+    trend: int,
+    variable: str,
+    node: int,
+    child_nodes_left: list[int],
+    child_nodes_right: list[int],
+) -> pd.DataFrame:
+    """Check monotonic trend is in place at a given split in a single tree.
 
-        raise TypeError("tree_df should be a pd.DataFrame")
+    This involves checking the maximum leaf prediction for the left child nodes against
+    the minimum leaf prediction for the right child nodes. E.g. for a monotonically
+    increasing trend (trend = 1) all the left child leaf nodes should be less than or
+    equal to all of the right child leaf nodes.
 
-    if not isinstance(tree_no, int):
+    Parameters
+    ----------
+    tree_df : pd.DataFrame
+        Single tree data. Subset of TabularTrees.trees.
 
-        raise TypeError("tree_no should be an int")
+    tree_no : int
+        Tree number of tree being checked.
 
-    if not isinstance(trend, int):
+    trend : int
+        Trend being checked. Either 1 for monotonically increasing and -1 for
+        monotonically decreasing trend.
 
-        raise TypeError("trend should be an int")
+    variable : str
+        Name of the variable being checked at the current node.
 
-    if not isinstance(node, int):
+    node : int
+        Node number being checked.
 
-        raise TypeError("node should be an int")
+    child_nodes_left : list[int]
+        List of {node}'s left child nodes.
 
-    if not isinstance(child_nodes_left, list):
+    child_nodes_right : list[int]
+        List of {node}'s right child nodes.
 
-        raise TypeError("child_nodes_left should be an list")
-
-    if not isinstance(child_nodes_right, list):
-
-        raise TypeError("child_nodes_right should be an list")
-
+    """
+    tree_nodes = tree_df["node"].tolist()
     all_child_nodes = child_nodes_left + child_nodes_right
 
-    tree_nodes = tree_df["node"].tolist()
-
     child_nodes_not_in_tree = list(set(all_child_nodes) - set(tree_nodes))
-
     if len(child_nodes_not_in_tree) > 0:
-
         raise ValueError(
-            "the following child nodes do not appear in tree; "
-            + str(child_nodes_not_in_tree)
+            f"the following child nodes do not appear in tree; {child_nodes_not_in_tree}"
         )
 
     left_nodes_max_pred = tree_df.loc[
@@ -498,35 +407,15 @@ def _check_monotonicity_at_split(
     ].min()
 
     if trend == 1:
-
         if left_nodes_max_pred <= right_nodes_min_pred:
-
             monotonic = True
-
         else:
-
             monotonic = False
-
-    elif trend == -1:
-
-        if left_nodes_max_pred >= right_nodes_min_pred:
-
-            monotonic = True
-
-        else:
-
-            monotonic = False
-
     else:
-
-        raise ValueError(
-            "unexpected value for trend; "
-            + str(trend)
-            + " variable; "
-            + str(variable)
-            + " node:"
-            + str(node)
-        )
+        if left_nodes_max_pred >= right_nodes_min_pred:
+            monotonic = True
+        else:
+            monotonic = False
 
     results = {
         "variable": variable,
