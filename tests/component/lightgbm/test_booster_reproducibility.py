d0ea84989ec5af01ac269ce64cdb9e5bb505dd3a
Richard Angell <richardangell37@gmail.com>
2024-08-25 13:33:56 +0100

    add more tests for EditableBooster

--- tests/component/lightgbm/test_booster_reproducibility.py
+++ tests/component/lightgbm/test_booster_reproducibility.py
@@ -1,6 +1,4 @@
-import lightgbm as lgb
 import numpy as np
-import pandas as pd
 
 from tabular_trees.lightgbm.booster_string import (
     BoosterString,
@@ -48,69 +46,49 @@ def test_booster_reproducible_from_editable_booster(diabetes_data, lgb_diabetes_
     np.testing.assert_array_equal(predictions, predictions_reproduced)
 
 
-def test_leaf_redictions_can_be_changed():
-    """Test that leaf predictions of an EditableBooster can be chnaged.
-
-    Specifically test that changes are able to propagate through the model and are
-    output by the Booster.predict method when the EditableBooster is converted back to a
-    Booster.
-
-    """
+def test_duplicating_trees_doubles_predictions(diabetes_data, lgb_diabetes_model):
+    """Test that duplicating all trees doubles predictions from the model."""
     # ARRANGE
 
-    data = pd.DataFrame(
-        {
-            "a": [1, 1, 0, 0],
-            "b": [1, 0, 1, 0],
-            "target": [25, 19, 9, 14],
-        }
-    )
+    predictions = lgb_diabetes_model.predict(diabetes_data["data"])
 
-    lgb_data = lgb.Dataset(
-        data=data[["a", "b"]],
-        label=data["target"],
-        feature_name=["a", "b"],
-    )
-
-    model = lgb.train(
-        params={
-            "verbosity": -1,
-            "max_depth": 3,
-            "objective": "regression",
-            "learning_rate": 1.0,
-            "min_data_in_leaf": 1,
-            "bagging_fraction": 1.0,
-            "feature_fraction": 1.0,
-            "min_gain_to_split": 0.0,
-            "min_sum_hessian_in_leaf": 0.0,
-            "seed": 0,
-        },
-        train_set=lgb_data,
-        num_boost_round=1,
-    )
-
-    original_predictions = model.predict(data[["a", "b"]])
-
-    editable_booster = EditableBooster.from_booster(model)
+    editable_booster = EditableBooster.from_booster(lgb_diabetes_model)
 
     # ACT
 
-    editable_booster.trees[0].leaf_value = [1, 2, 3, 4]
+    editable_booster.trees.extend(editable_booster.trees[:])
+    editable_booster.header.tree_sizes.extend(editable_booster.header.tree_sizes)
 
-    updated_booster = editable_booster.to_booster()
-
-    new_predictions = updated_booster.predict(data[["a", "b"]])
+    doubled_booster = editable_booster.to_booster()
 
     # ASSERT
 
-    np.testing.assert_array_almost_equal(
-        original_predictions,
-        np.array([25, 19, 9, 14]),
-        decimal=14,
-    )
+    new_predictions = doubled_booster.predict(diabetes_data["data"])
 
-    np.testing.assert_array_almost_equal(
-        new_predictions,
-        np.array([3, 2, 4, 1]),
-        decimal=14,
-    )
+    np.testing.assert_array_almost_equal(2 * predictions, new_predictions, decimal=11)
+
+
+def test_shrinkage_does_not_change_predictions(diabetes_data, lgb_diabetes_model):
+    """Demonstrate that changing the shrinkage does not change Booster predictions."""
+    # ARRANGE
+
+    predictions = lgb_diabetes_model.predict(diabetes_data["data"])
+
+    editable_booster = EditableBooster.from_booster(lgb_diabetes_model)
+
+    # ACT
+
+    for tree in editable_booster.trees:
+        tree.shrinkage = 0.11
+
+    editable_booster.header.tree_sizes = [
+        len(tree.get_booster_sting()) + 1 for tree in editable_booster.trees
+    ]
+
+    booster_with_shrinkage = editable_booster.to_booster()
+
+    # ASSERT
+
+    new_predictions = booster_with_shrinkage.predict(diabetes_data["data"])
+
+    np.testing.assert_array_equal(predictions, new_predictions)
